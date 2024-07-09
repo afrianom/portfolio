a712c4eaa6ca29acaea426f8d617a7e504ab1f24
piperiao <andresfeliperianom@gmail.com>
2024-07-08 21:55:28 -0500

    put the linkedIn and gitHub buttons, with the link to the pages, and end the about me section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -24,7 +24,7 @@ const About = () => {
                     <h2>Work Experience</h2>
                         <div className="work-1">
                             <h3>Web Designer</h3>
-                            <p><a href="https://gbmcomunicaciones.com/">GBM comunicaciones</a> | Nov 2021 - Nov 2023</p>
+                            <p><a href="https://gbmcomunicaciones.com/" target="_blank" rel="noopener noreferrer">GBM comunicaciones</a> | Nov 2021 - Nov 2023</p>
                             <p>I was responsible for designing and animating projects according to client requirements.</p>
                         </div>
                         <div className="work-1">
@@ -56,15 +56,15 @@ const About = () => {
                 <div className="bottom-right">
                     <h2>Languages</h2>
                     <ul>
-                        <li>English<span>Fluent</span></li>
-                        <li>Spanish<span>Native</span></li>
-                        <li>German<span>Basic</span></li>
+                        <li>English   <span>Fluent</span></li>
+                        <li>Spanish   <span>Native</span></li>
+                        <li>German   <span>Basic</span></li>
                     </ul>
                 </div>
             </div>
             <div className="skills">
                 <h2>Stack</h2>
-                <div>
+                <div className="skills-lines">
                     <div className="stack-l1">
                         <div className="stack-div">
                             <img src={reactLogo} width='50' alt="react logo" />
@@ -84,11 +84,9 @@ const About = () => {
                         </div>
                         <div className="stack-div">
                             <img src={postgresqlLogo} width='50' alt="postgresql logo" />
-                            <p>PostgrSQL</p>
+                            <p>PostgreSQL</p>
                         </div>
                     </div>
-                </div>
-                <div>
                     <div className="stack-l2">
                         <div className="stack-div">
                             <img src={htmllLogo} alt="html logo" width='50' />
@@ -108,11 +106,10 @@ const About = () => {
                         </div>
                         <div className="stack-div">
                             <img src={adobeLogo} alt="adobe logo" width='50' />
-                            <p>Adobe Creative Studio</p>
+                            <p className="adobe-studio">Adobe Creative<br/> Studio</p>
                         </div>
                     </div>
-                </div>
-                
+                </div>                
             </div>
         </div>
     )
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import './hero.css'
 import profile_pic from '../../assets/profile-pic.jpg'
+import LinkedInIcon from "./LinkedInIcon.jsx";
+import GitHubIcon from './GitHubIcon.jsx';
 
-
-const About = () => {
+const Hero = () => {
     return (
         <div className="hero">
             <div className="hero-first">
-                <img src={profile_pic} alt="profile picture" />
+                <img className="profile-pic" src={profile_pic} alt="profile picture" />
                 <h1>Hello, i'm Andres, <span>fullstack developer.</span></h1>
             </div>
             <div className="hero-second">
                 <p>I'm a full-stack developer with one year of experience, enthusiastic about creating impactful projects. I am committed to continuous learning and innovation in every project I undertake.</p>
-                <div className="my-resume">My resume</div>
+                <div className="hero-buttons">
+                    <a href="https://www.linkedin.com/in/andres-ria%C3%B1o-193003251/" target="_blank" rel="noopener noreferrer"><LinkedInIcon className='socialmedia-btn'/></a>
+                    <a href="https://github.com/afrianom" target="_blank" rel="noopener noreferrer"><GitHubIcon className='socialmedia-btn'/></a>
+                    {/* <object type="image/svg+xml" data={linkedinlogo} width="50" className="linked-in-btn"></object> */}
+                    {/* <object src={githublogo} alt="github logo" className="github-btn" width='50'></object> */}
+                    <div className="my-resume">My resume</div>
+                </div>
             </div>
         </div>
     )
 }
 
-export default About
+export default Hero;
